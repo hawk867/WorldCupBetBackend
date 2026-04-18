d6a33049bd6fffe92a7d36cc7c7897ab8c32b26c
Daniel Esteban Cepeda Rojas <whisper1831@hotmail.com>
2026-04-17 21:02:45 -0500

    Initial commit

---
+++ src/main/java/org/danielesteban/worldcupbetbackend/WorldCupBetBackendApplication.java
@@ -0,0 +1,13 @@
+package org.danielesteban.worldcupbetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldCupBetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldCupBetBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/danielesteban/worldcupbetbackend/WorldCupBetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.danielesteban.worldcupbetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldCupBetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
